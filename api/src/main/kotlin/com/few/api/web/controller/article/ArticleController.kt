005744abaca02ef1d6d50286597b950028633fef
belljun3395 <195850@jnu.ac.kr>
2025-01-13 10:57:48 +0900

    test: 코루틴 테스트용 커밋 - controller

--- api/src/main/kotlin/com/few/api/web/controller/article/ArticleController.kt
+++ api/src/main/kotlin/com/few/api/web/controller/article/ArticleController.kt
@@ -80,31 +80,40 @@ class ArticleController(
             required = false,
             defaultValue = "-1"
         ) categoryCd: Byte,
-    ): ApiResponse<ApiResponse.SuccessBody<ReadArticlesResponse>> {
-        val useCaseOut = runBlocking {
-            browseArticlesUseCase.execute(ReadArticlesUseCaseIn(prevArticleId, categoryCd))
+    ): CompletableDeferred<ApiResponse<ApiResponse.SuccessBody<ReadArticlesResponse>>> {
+        val deferredResult = CompletableDeferred<ApiResponse<ApiResponse.SuccessBody<ReadArticlesResponse>>>()
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val useCaseOut = browseArticlesUseCase.execute(ReadArticlesUseCaseIn(prevArticleId, categoryCd))
+                val articles: List<ReadArticleResponse> =
+                    useCaseOut.articles
+                        .map { a ->
+                            ReadArticleResponse(
+                                id = a.id,
+                                title = a.title,
+                                writer =
+                                WriterInfo(
+                                    id = a.writer.id,
+                                    name = a.writer.name,
+                                    url = a.writer.url,
+                                    imageUrl = a.writer.imageUrl
+                                ),
+                                mainImageUrl = a.mainImageUrl,
+                                content = a.content,
+                                problemIds = a.problemIds,
+                                category = a.category,
+                                createdAt = a.createdAt,
+                                views = a.views,
+                                workbooks = a.workbooks.map { WorkbookInfo(it.id, it.title) }
+                            )
+                        }.toList()
+                val response = ReadArticlesResponse(articles, useCaseOut.isLast)
+                deferredResult.complete(ApiResponseGenerator.success(response, HttpStatus.OK))
+            } catch (e: Exception) {
+                deferredResult.completeExceptionally(e)
+            }
         }
-        val articles: List<ReadArticleResponse> = useCaseOut.articles.map { a ->
-            ReadArticleResponse(
-                id = a.id,
-                title = a.title,
-                writer = WriterInfo(
-                    id = a.writer.id,
-                    name = a.writer.name,
-                    url = a.writer.url,
-                    imageUrl = a.writer.imageUrl
-                ),
-                mainImageUrl = a.mainImageUrl,
-                content = a.content,
-                problemIds = a.problemIds,
-                category = a.category,
-                createdAt = a.createdAt,
-                views = a.views,
-                workbooks = a.workbooks.map { WorkbookInfo(it.id, it.title) }
-            )
-        }
-        val response = ReadArticlesResponse(articles, useCaseOut.isLast)
-        return ApiResponseGenerator.success(response, HttpStatus.OK)
+        return deferredResult
     }
 
     @GetMapping("/categories")
